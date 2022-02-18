451d1165b20a227a912a36d75e6c23c89b298504
kjk6690 <70821299+kjk6690@users.noreply.github.com>
2022-02-17 19:08:44 -0500

    Changed files into functions, and added main.py to call other functions

---
+++ draw_graph_from_file.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def draw_graph_from_file(filename,circular=False):
+    """
+    Given an adjacency list representation of a graph, draw the graph
+    Note: May take a long time if Graph is large
+    :param filename: filename of adjacency list
+    :param circular: Plot in circular format. Will draw graph faster
+    """
+    plt.figure()
+    G = nx.read_adjlist(filename)
+    print("Made Graph")
+    if circular:
+        nx.draw_circular(G, with_labels=False, node_size=2, linewidths=.5)
+    else:
+        nx.draw_networkx(G, with_labels=False, node_size=2, linewidths=.5)
+    print("Drawn")
+    plt.show()
---
+++ main.py
@@ -0,0 +1,3 @@
+from draw_graph_from_file import *
+
+draw_graph_from_file("example_block.adjlist")
