5b42e3c6212082106162e361a0aec4fb3083541d
kjk6690 <70821299+kjk6690@users.noreply.github.com>
2022-03-31 11:21:18 -0400

    Added functionality
    Functionality added:
    Get N-th previous block function
    Error handling
    Metrics: graph_supernode_flow, graph_diameter, graph_SCC_orders, print_all_metrics

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import networkx as nx
+
 from toolbox import *
 
 if __name__ == "__main__":
     bpd = 144 # number of blocks per day on average
     # blockhash = get_latest_block_hash()
-    download_n_blocks('0000000000000000000521bdfff05eb8e3f9b9c5c172e9a0b651498a1028b61b', bpd)
-
-
+    # download_n_blocks('00000000000000000001109f4bf0bcbecea18136ffe0a8ffb437c2c0b6ac433f', bpd)
+    G = nx.read_adjlist("./data/block_729448_144.adjlist", create_using=nx.DiGraph)
+    # G = nx.read_adjlist("./data/block_729655_144.adjlist", create_using=nx.DiGraph)
+    print_all_metrics(G)
--- toolbox.py
+++ toolbox.py
@@ -6,6 +6,7 @@ from varname import nameof
 import os
 import matplotlib.pyplot as plt
 import statistics as stats
+import pandas as pd
 
 url = "https://blockchain.info/"
 
@@ -37,6 +38,18 @@ def get_tx(tx_hash):
     response = requests.get(url + "rawtx/" + tx_hash)
     if response.status_code == 200:  # If request is successful, continue
         return response.json() # Return tx
+
+def get_nth_prev_block(blockhash, n):
+    """
+    Gets nth previous block on the block from a given hash
+    :param blockhash: starting blockhash
+    :param n: number of blocks down
+    :return: block data for the nth previous block
+    """
+    block = get_block(blockhash)
+    for i in range(0,n):
+        block = get_block(block['prev_block'])
+    return block
 
 def create_transaction_CBG(tx_details):
     """
@@ -80,11 +93,14 @@ def create_block_graph(block, progress_updates=True):
     for tx in txs:
         if progress_updates:
             print("{:3.2f}%".format(counter*100/num_tx))
-        tx_details = get_tx(tx['hash'])
-        CBG = create_transaction_CBG(tx_details)
-        G.add_nodes_from(CBG)
-        G.add_edges_from(CBG.edges())
-        counter += 1
+            try:
+                tx_details = get_tx(tx['hash'])
+                CBG = create_transaction_CBG(tx_details)
+                G.add_nodes_from(CBG)
+                G.add_edges_from(CBG.edges())
+            except:
+                print("Error @ tx {:s}, block {:s} ".format(tx['hash'], block['hash']))
+            counter += 1
     return G
 
 
@@ -109,15 +125,18 @@ def download_n_blocks(first_block_hash, n, progress_updates=True):
     nx.write_adjlist(G,"./data/block_"+height+"_0.adjlist")
     for i in range(0,n):
         if progress_updates:
-            print(" {:d} blocks processed out of {:d} ".format(i,n))
-        subgraph = create_block_graph(block, progress_updates=progress_updates)
-        G = nx.read_adjlist("./data/block_"+height+"_"+str(i)+".adjlist")
-        G.add_nodes_from(subgraph)
-        G.add_edges_from(subgraph.edges())
-        nx.write_adjlist(G, "./data/block_"+height+"_"+str(i+1)+".adjlist")
-        del G; os.remove("./data/block_"+height+"_"+str(i)+".adjlist")
-        block_hash = block['prev_block']
-        if i < n: block = get_block(block_hash)
+            print("{:d} blocks processed out of {:d} ".format(i,n))
+            print("block hash {:s}".format(block_hash))
+        try:
+            subgraph = create_block_graph(block, progress_updates=progress_updates)
+            G = nx.read_adjlist("./data/block_"+height+"_"+str(i)+".adjlist")
+            G.add_nodes_from(subgraph)
+            G.add_edges_from(subgraph.edges())
+            nx.write_adjlist(G, "./data/block_"+height+"_"+str(i+1)+".adjlist")
+            del G; os.remove("./data/block_"+height+"_"+str(i)+".adjlist")
+            block_hash = block['prev_block']
+            if i < n: block = get_block(block_hash)
+        except: print("Error @ block {:s} tx ".format(block_hash))
     if progress_updates:
         print(" {:d} blocks processed out of {:d} ".format(n, n))
 
@@ -144,24 +163,63 @@ def draw_graph_from_file(filename,circular=False,with_labels=False, create_using
 
 ######### METRICS ################
 def graph_density(G):
+    """ Returns density of the given graph"""
     return nx.density(G)
 
+def graph_supernode_flow(G):
+    """ Returns number of nodes that are reachable by the mining reward supernode"""
+    if '-1' in G.nodes():
+        BFS_tree = nx.bfs_tree(G,'-1')
+        return len(BFS_tree.nodes())/len(G.nodes())
+    elif -1 in G.nodes():
+        BFS_tree = nx.bfs_tree(G, -1)
+        return len(BFS_tree.nodes()) / len(G.nodes())
+    else: return -1.0
+
+def graph_diameter(G):
+    """ Returns maximum diameter across each strongly connected component"""
+    max_diam = 0
+    for c in nx.strongly_connected_components(G):
+        conn_subgraph = G.subgraph(c).copy()
+        max_diam = max(max_diam, nx.diameter(conn_subgraph))
+    return max_diam
+
 def graph_SCC_orders(G):
-    return nx.strongly_connected_components(G)
+    """ Returns list of SCC orders"""
+    return [len(c) for c in sorted(nx.strongly_connected_components(G), key=len, reverse=True)]
 
 def graph_SCC_mean(G):
+    """ Returns mean of SCC orders"""
     SCC_orders=graph_SCC_orders(G)
     return stats.mean(SCC_orders)
 
 def graph_SCC_median(G):
+    """ Returns median of SCC orders"""
     return stats.median(graph_SCC_orders(G))
 
 def graph_SCC_max(G):
+    """ Returns max of SCC orders"""
     return max(graph_SCC_orders(G))
 
-def graph_supernode_flow(G):
-    BFS_tree = nx.bfs_tree(G,-1)
-    return len(BFS_tree.nodes())/len(G.nodes())
+def print_all_metrics(G):
+    """
+    Prints all metrics about graph
+    :param G: Graph to judge metrics on
+    """
+    print("Density:",graph_density(G)*100,"%")
+    print("Supernode flow: {:3.2f}%".format(graph_supernode_flow(G)*100))
+    SCC = nx.strongly_connected_components(G)
+    # max_diam = 0
+    # for c in SCC:
+    #     conn_subgraph = G.subgraph(c).copy()
+    #     max_diam = max(max_diam, nx.diameter(conn_subgraph))
+    # print("Diameter:", max_diam)
+    SCC_orders = [len(c) for c in SCC]
+    df= pd.DataFrame({'SCC_orders':SCC_orders})
+    print( df.SCC_orders.value_counts().reset_index().rename(
+           columns={'index': 'SCC Orders', 'SCC_orders': 'count'}).sort_values(by='SCC Orders', ascending=False))
+    print("SCC Order Mean:", stats.mean(SCC_orders))
+    print("SCC Order Median:", stats.median(SCC_orders))
+    print("SCC Order Max:", max(SCC_orders))
 
-def graph_diameter(G):
-    return nx.diameter(G)
+
